ae739fc5a5fe2777acb065092dc448ac58e707d1
Ralf Lang <ralf.lang@ralf-lang.de>
2022-09-08 10:54:02 +0000

    Ensure we can require php versions which are not installed

--- script/release-tagger.sh
+++ script/release-tagger.sh
@@ -4,7 +4,7 @@ git checkout main
 git branch -d release-ephemeral
 git branch release-ephemeral
 git checkout release-ephemeral
-composer require php "^8.1"
+composer require php "^8.1" --ignore-platform-reqs
 git add composer.json
 git commit -m "Fixup composer.json for 8.1 release"
 git tag v1.0.0.80100
@@ -13,7 +13,7 @@ git checkout main
 git branch -d release-ephemeral
 git branch release-ephemeral
 git checkout release-ephemeral
-composer require php "^7.4"
+composer require php "^7.4" --ignore-platform-reqs
 git add composer.json
 git commit -m "Fixup composer.json for 8.1 release"
 git tag v1.0.0.70400
